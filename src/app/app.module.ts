7368c90f563fd69b7d6779b258207ababc9f9fde
bfox6 <bfox6#nd.edu>
2017-01-24 00:07:39 -0500

    Redesigned page structure and added about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about.component';
+import { MainComponent } from './main.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'about',  component: AboutComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
+
+
